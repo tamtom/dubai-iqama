c92d757da84ce9be18fecc8b5f30cf3484503794
tamtom <omar.tamimid1@gmail.com>
2026-01-21 17:17:47 +0400

    Initial Commit

---
+++ Uae iqama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uae iqama
+//
+//  Created by Omar Altamimi on 21/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uae iqama/Uae_iqamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uae_iqamaApp.swift
+//  Uae iqama
+//
+//  Created by Omar Altamimi on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Uae_iqamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
